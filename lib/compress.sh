a3d922b7384a31a8ba89e198c958920e4b4ea9a6
Emmanuel Garcia <blasten@gmail.com>
2012-07-12 15:05:35 -0400

    4th release

---
+++ lib/compress.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+# sudo npm install -g uglify-js
+
+IN=turn.js
+OUT=turn.min.js
+
+SIZE_MIN=$(uglifyjs "$IN" --extra --unsafe | tee "$OUT" | wc -c)
+SIZE_GZIP=$(gzip -nfc --best "$OUT" | wc -c)
+
+echo $SIZE_MIN bytes minified, $SIZE_GZIP bytes gzipped
+
+if [ "$1" == "--test" ]; then
+  rm "$OUT"
+fi
